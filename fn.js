f0a74392b0cc878e21405879dafa8b8513de0a46
Jo <last2599@gmail.com>
2021-09-21 07:31:56 +0900

    정규식  / 배열 / 에러발생 테스트

--- fn.js
+++ fn.js
@@ -1,6 +1,9 @@
 const fn = {
   add: (num1, num2) => num1 + num2,
   makeUser: (name, age) => ({ name, age }),
+  throwErr: () => {
+    throw new Error("error");
+  },
 };
 
 module.exports = fn;
--- fn.test.js
+++ fn.test.js
@@ -43,3 +43,15 @@ test("ID는 10자 이하여야 합니다.", () => {
 test("0.1 더하기 0.2 는 0.3 입니다.", () => {
   expect(fn.add(0.1, 0.2)).toBeCloseTo(0.3);
 });
+test("Hollow Wolrd 에 h가 존재 하는가?", () => {
+  expect("Hello World").toMatch(/h/i);
+});
+test("유저 리스트에 Mike 가 있는가?", () => {
+  const user = "Mike";
+  const userList = ["Tom", "Jane", "Mike"];
+  expect(userList).toContain(user);
+});
+
+test("에러가 발생했는가?", () => {
+  expect(() => fn.throwErr()).toThrow("error");
+});
