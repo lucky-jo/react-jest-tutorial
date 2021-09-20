694158778a7ecc4a289c0a3048e44b93d226377d
Jo <last2599@gmail.com>
2021-09-21 07:16:55 +0900

    객체를 비교 할 경우, toStrictEqual 을 사용을 권장

--- fn.js
+++ fn.js
@@ -1,5 +1,6 @@
 const fn = {
   add: (num1, num2) => num1 + num2,
+  makeUser: (name, age) => ({ name, age }),
 };
 
 module.exports = fn;
--- fn.test.js
+++ fn.test.js
@@ -11,3 +11,10 @@ test("2 더하기 3은 5이다.", () => {
 test("3 더하기 3은 5이 아니다.", () => {
   expect(fn.add(3, 3)).not.toBe(5);
 });
+
+test("이름과 나이를 전달받아서 객체를 반환한다.", () => {
+  expect(fn.makeUser("Mike", 30)).toStrictEqual({
+    name: "Mike",
+    age: 30,
+  });
+});
