7042c91636749ced63a4f33e173788faf30a4e6b
Jo <last2599@gmail.com>
2021-09-22 01:59:05 +0900

    스냅샷

--- src/component/Hello.test.jsx
+++ src/component/Hello.test.jsx
@@ -2,12 +2,25 @@ import { render, screen } from "@testing-library/react";
 import Hello from "./Hello";
 
 const user = {
-  name: "Mike",
+  name: "Tom",
   age: 30,
+};
+const user2 = {
+  // name: "Mike",
+  age: 20,
 };
 
 test("Mike 라는 글자가 포함되는가?", () => {
   render(<Hello user={user} />);
-  const helloEl = screen.getByText(/Mike/);
+  const helloEl = screen.getByText(/Tom/);
   expect(helloEl).toBeInTheDocument();
+});
+
+test("snapshot : name 있음", () => {
+  const el = render(<Hello user={user} />);
+  expect(el).toMatchSnapshot();
+});
+test("snapshot : name 없음", () => {
+  const el = render(<Hello />);
+  expect(el).toMatchSnapshot();
 });
