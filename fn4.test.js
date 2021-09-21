de9e2051d3af7125ed4a0d219f4f8a7dbbddb380
Jo <last2599@gmail.com>
2021-09-21 22:27:48 +0900

    mock 함수의 호출 횟수와 인자 값을 확인하는 테스트

--- fn4.test.js
+++ fn4.test.js
@@ -9,3 +9,10 @@ test("dd", () => {
   console.log(mockFn.mock.calls);
   expect(1).toBe(1);
 });
+
+test("함수는 2번 호출됩니다.", () => {
+  expect(mockFn.mock.calls.length).toBe(2);
+});
+test("2번째로 호출된 함수에 전달된 첫번째 인수는 1 입니다.", () => {
+  expect(mockFn.mock.calls[1][0]).toBe(1);
+});
